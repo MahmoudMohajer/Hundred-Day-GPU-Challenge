ac4c1831c814c605ea0b1413042521b4d8f17d22
Mahmoud Mohajer <mahmohajer3@gmail.com>
2025-02-08 12:18:27 +0430

    adding error handling

--- Day003/vector_add.cu
+++ Day003/vector_add.cu
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define cudaCheckError(call) \
+    do { \
+        cudaError_t err = call; \
+        if (err != cudaSuccess) { \
+            fprintf(stderr, "CUDA error in %s at line %d: %s\n", __FILE__, __LINE__, \
+                                                    cudaGetErrorString(err)); \
+            exit(EXIT_FAILURE);                                      \
+        }                                                            \
+    } while (0);
+    
+
+
 __global__ void vectorAdd(const float *a, const float *b,float *c, int n) {
     int idx = blockDim.x * blockIdx.x + threadIdx.x; 
 
@@ -23,27 +35,28 @@ int main() {
 
     // allocating memory for host arrays 
     float *a_d, *b_d, *c_d; 
-    cudaMalloc((void**)&a_d, bytes);
-    cudaMalloc((void**)&b_d, bytes);
-    cudaMalloc((void**)&c_d, bytes);
+    cudaCheckError(cudaMalloc((void**)&a_d, bytes));
+    cudaCheckError(cudaMalloc((void**)&b_d, bytes));
+    cudaCheckError(cudaMalloc((void**)&c_d, bytes));
 
     // copy to vram 
-    cudaMemcpy(a_d, a_h, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(b_d, b_h, bytes, cudaMemcpyHostToDevice);
+    cudaCheckError(cudaMemcpy(a_d, a_h, bytes, cudaMemcpyHostToDevice));
+    cudaCheckError(cudaMemcpy(b_d, b_h, bytes, cudaMemcpyHostToDevice));
 
     int threadsPerBlock = 256;
     int BlocksPerGrid = (size + threadsPerBlock -1) / threadsPerBlock;
 
     // launching the kernel 
     vectorAdd<<<BlocksPerGrid, threadsPerBlock>>>(a_d, b_d, c_d, size);
+    cudaCheckError(cudaGetLastError());
 
-    cudaDeviceSynchronize();
+    cudaCheckError(cudaDeviceSynchronize());
 
-    cudaMemcpy(c_h, c_d, bytes, cudaMemcpyDeviceToHost);
+    cudaCheckError(cudaMemcpy(c_h, c_d, bytes, cudaMemcpyDeviceToHost));
 
-    cudaFree(a_d);
-    cudaFree(b_d);
-    cudaFree(c_d);
+    cudaCheckError(cudaFree(a_d));
+    cudaCheckError(cudaFree(b_d));
+    cudaCheckError(cudaFree(c_d));
 
     for(int i = 0; i < size; i++) {
         if (c_h[i] != a_h[i] + b_h[i]){
